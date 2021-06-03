ed3a8a479493a43f4a3dbaac831b6ec7dc49f991
Nicko Guyer <nicko.guyer@kaleido.io>
2021-06-03 13:59:53 -0400

    Add depends_on for ethconnect
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

--- internal/stacks/dockerConfig.go
+++ internal/stacks/dockerConfig.go
@@ -67,11 +67,14 @@ func CreateDockerCompose(stack *Stack) *DockerComposeConfig {
 
 	for _, member := range stack.Members {
 		compose.Services["firefly_core_"+member.ID] = &Service{
-			Image:     "kaleidoinc/firefly",
-			Ports:     []string{fmt.Sprint(member.ExposedFireflyPort) + ":5000"},
-			Volumes:   []string{path.Join(stackDir, "firefly_"+member.ID+".core") + ":/etc/firefly/firefly.core"},
-			DependsOn: map[string]map[string]string{"postgres_" + member.ID: {"condition": "service_healthy"}},
-			Logging:   standardLogOptions,
+			Image:   "kaleidoinc/firefly",
+			Ports:   []string{fmt.Sprint(member.ExposedFireflyPort) + ":5000"},
+			Volumes: []string{path.Join(stackDir, "firefly_"+member.ID+".core") + ":/etc/firefly/firefly.core"},
+			DependsOn: map[string]map[string]string{
+				"postgres_" + member.ID:   {"condition": "service_healthy"},
+				"ethconnect_" + member.ID: {"condition": "service_started"},
+			},
+			Logging: standardLogOptions,
 		}
 
 		compose.Services["postgres_"+member.ID] = &Service{
