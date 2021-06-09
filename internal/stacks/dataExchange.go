11c717fe95f58da18c2b0ce42b38d38c1ee88f14
Nicko Guyer <nicko.guyer@kaleido.io>
2021-06-09 14:06:34 -0400

    Remove unused cert code
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

--- internal/stacks/dataExchange.go
+++ internal/stacks/dataExchange.go
@@ -1,16 +1,7 @@
 package stacks
 
 import (
-	"bytes"
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
 	"fmt"
-	"math/big"
-	"net"
-	"time"
 )
 
 type DataExchangeListenerConfig struct {
@@ -31,20 +22,6 @@ type DataExchangePeerConfig struct {
 }
 
 func (s *Stack) GenerateDataExchangeHTTPSConfig(memberId string) *DataExchangePeerConfig {
-
-	peers := make([]*PeerConfig, len(s.Members)-1)
-	i := 0
-
-	for _, member := range s.Members {
-		if member.ID != memberId {
-			peers[i] = &PeerConfig{
-				ID:       member.ID,
-				Endpoint: fmt.Sprintf("https://dataexchange_%s:3001", member.ID),
-			}
-
-		}
-	}
-
 	return &DataExchangePeerConfig{
 		API: &DataExchangeListenerConfig{
 			Hostname: "0.0.0.0",
@@ -57,85 +34,4 @@ func (s *Stack) GenerateDataExchangeHTTPSConfig(memberId string) *DataExchangePe
 		},
 		Peers: []*PeerConfig{},
 	}
-}
-
-func CreateCert(hostname, memberId string) (*bytes.Buffer, *bytes.Buffer, error) {
-	ca := &x509.Certificate{
-		SerialNumber: big.NewInt(2019),
-		Subject: pkix.Name{
-			CommonName:   hostname,
-			Organization: []string{memberId},
-		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(10, 0, 0),
-		IsCA:                  true,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-		BasicConstraintsValid: true,
-	}
-
-	caPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPrivKey.PublicKey, caPrivKey)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	caPEM := new(bytes.Buffer)
-	pem.Encode(caPEM, &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: caBytes,
-	})
-
-	caPrivKeyPEM := new(bytes.Buffer)
-	pem.Encode(caPrivKeyPEM, &pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(caPrivKey),
-	})
-
-	cert := &x509.Certificate{
-		SerialNumber: big.NewInt(1658),
-		Subject: pkix.Name{
-			CommonName:   "localhost",
-			Organization: []string{memberId},
-		},
-		IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().AddDate(10, 0, 0),
-		SubjectKeyId: []byte{1, 2, 3, 4, 6},
-		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
-		KeyUsage:     x509.KeyUsageDigitalSignature,
-	}
-
-	certPrivKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	certBytes, err := x509.CreateCertificate(rand.Reader, cert, ca, &certPrivKey.PublicKey, caPrivKey)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	certPEM := new(bytes.Buffer)
-	pem.Encode(certPEM, &pem.Block{
-		Type:  "CERTIFICATE",
-		Bytes: certBytes,
-	})
-
-	privKeyBytes := x509.MarshalPKCS1PrivateKey(certPrivKey)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	certPrivKeyPEM := new(bytes.Buffer)
-	pem.Encode(certPrivKeyPEM, &pem.Block{
-		Type:  "PRIVATE KEY",
-		Bytes: privKeyBytes,
-	})
-
-	return certPEM, certPrivKeyPEM, nil
 }
