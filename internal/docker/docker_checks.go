c5c303b922cff13e616c50321550676ceac07fa7
sukrut29 <sukrut.sathaye@sttech.de>
2021-11-02 17:02:43 +0100

    Changed back to capital A
    Signed-off-by: sukrut29 <sukrut.sathaye@sttech.de>

--- internal/docker/docker_checks.go
+++ internal/docker/docker_checks.go
@@ -11,20 +11,20 @@ func CheckDockerConfig() error {
 	dockerCmd := exec.Command("docker", "-v")
 	_, err := dockerCmd.Output()
 	if err != nil {
-		return fmt.Errorf("an error occurred while running docker. Is docker installed on your computer?")
+		return fmt.Errorf("An error occurred while running docker. Is docker installed on your computer?")
 	}
 
 	dockerComposeCmd := exec.Command("docker-compose", "-v")
 	_, err = dockerComposeCmd.Output()
 
 	if err != nil {
-		return fmt.Errorf("an error occurred while running docker-compose. Is docker-compose installed on your computer?")
+		return fmt.Errorf("An error occurred while running docker-compose. Is docker-compose installed on your computer?")
 	}
 
 	dockerDeamonCheck := exec.Command("docker", "ps")
 	_, err = dockerDeamonCheck.Output()
 	if err != nil {
-		return fmt.Errorf("an error occurred while running docker. Is docker running on your computer?")
+		return fmt.Errorf("An error occurred while running docker. Is docker running on your computer?")
 	}
 
 	return nil
