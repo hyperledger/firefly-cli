581ea6f2f0bbd5d1439f47d99beadcd070c78ff8
hfuss <haydenfuss@gmail.com>
2022-01-06 17:15:19 -0500

    version Command
    Signed-off-by: hfuss <haydenfuss@gmail.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# Copyright © 2021 Kaleido, Inc.
+# Copyright © 2022 Kaleido, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -16,11 +16,12 @@
 
 VGO=go
 GOBIN := $(shell $(VGO) env GOPATH)/bin
+VERSION := $(shell git rev-parse --short HEAD)
 LINT := $(GOBIN)/golangci-lint
 
 all: build
 build: ## Builds all go code
-		cd ff && go build
+		cd ff && go build -ldflags="-X 'github.com/hyperledger/firefly-cli/internal/version.Version=$(VERSION)'"
 install: ## Installs the package
 		cd ff && go install
 
---
+++ cmd/version.go
@@ -0,0 +1,37 @@
+// Copyright © 2022 Kaleido, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/hyperledger/firefly-cli/internal/version"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the version",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Printf("Version: %s\n", version.Version)
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
