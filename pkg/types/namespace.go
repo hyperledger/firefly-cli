06921c36d230972114474d887382be333cc00cd2
Nicko Guyer <nicko.guyer@kaleido.io>
2022-06-30 08:46:21 -0400

    Fix defaultkey caplitalization to make Viper happy
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

--- pkg/types/namespace.go
+++ pkg/types/namespace.go
@@ -22,7 +22,7 @@ type Namespace struct {
 	Description string            `yaml:"description,omitempty"`
 	Plugins     []string          `yaml:"plugins"`
 	Multiparty  *MultipartyConfig `yaml:"multiparty,omitempty"`
-	DefaultKey  interface{}       `yaml:"defaultKey"`
+	DefaultKey  interface{}       `yaml:"defaultkey"`
 }
 
 type Plugins struct {
