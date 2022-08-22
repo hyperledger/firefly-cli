4e5b53bed7aaafa16e0aa41a96c23c8b26954610
Nicko Guyer <nicko.guyer@kaleido.io>
2022-08-22 17:34:19 -0400

    Remove deprecated ethconnect deployment code
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

--- internal/blockchain/ethereum/remoterpc/remoterpc_provider.go
+++ internal/blockchain/ethereum/remoterpc/remoterpc_provider.go
@@ -136,7 +136,11 @@ func (p *RemoteRPCProvider) GetContracts(filename string, extraArgs []string) ([
 }
 
 func (p *RemoteRPCProvider) DeployContract(filename, contractName, instanceName string, member *types.Organization, extraArgs []string) (*types.ContractDeploymentResult, error) {
-	return nil, fmt.Errorf("contract deployment not supported for Remote RPC URL connections")
+	contracts, err := ethereum.ReadContractJSON(filename)
+	if err != nil {
+		return nil, err
+	}
+	return p.connector.DeployContract(contracts.Contracts[contractName], instanceName, member, extraArgs)
 }
 
 func (p *RemoteRPCProvider) CreateAccount(args []string) (interface{}, error) {
