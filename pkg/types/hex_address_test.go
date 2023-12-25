b4b28cd3ec26e56718d8398e709244bb0b659d08
Philip-21 <philipuzomaobiora@gmail.com>
2023-12-25 21:04:16 +0100

    testcases
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- pkg/types/hex_address_test.go
+++ pkg/types/hex_address_test.go
@@ -4,7 +4,9 @@ import (
 	"encoding/hex"
 	"testing"
 
+	"github.com/hyperledger/firefly-signer/pkg/ethtypes"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 )
 
 func TestWrapHexAddress(t *testing.T) {
@@ -12,6 +14,10 @@ func TestWrapHexAddress(t *testing.T) {
 		Name     string
 		Hexvalue string `yaml:"hexvalue"`
 	}{
+		{
+			Name:     "Case 16-Bits",
+			Hexvalue: "0x1f2a000000000000000000000000000000000000",
+		},
 		{
 			Name:     "Case 24-Bits",
 			Hexvalue: "0x123abc0000000000000000000000000000000000",
@@ -25,20 +31,16 @@ func TestWrapHexAddress(t *testing.T) {
 			Hexvalue: "0x1234567890abcdef012345670000000000000000",
 		},
 		{
+			Name:     "Case 65-Bits",
+			Hexvalue: "0x1234567890abcdef0123456789abcdef6789abcd",
+		},
+		{
 			Name:     "Case 128-Bits",
 			Hexvalue: "0x1234567890abcdef0123456789abcdef00000000",
 		},
 		{
 			Name:     "Case 152-Bits",
 			Hexvalue: "0x549b5f43a40e1a0522864a004cfff2b0ca473a65",
-		},
-		{
-			Name:     "Case 16-Bits",
-			Hexvalue: "0x1f2a000000000000000000000000000000000000",
-		},
-		{
-			Name:     "Case 65-Bits",
-			Hexvalue: "0x1234567890abcdef0123456789abcdef6789abcd",
 		},
 	}
 	for _, tc := range tests {
@@ -66,7 +68,7 @@ func TestWrapHexAddress(t *testing.T) {
 	}
 }
 
-//Wrong values to test the accuracy of the  WrapHexAddress()
+// Wrong values to test the accuracy of the  WrapHexAddress()
 func TestFailWrapHexAddress(t *testing.T) {
 	testData := []struct {
 		Name     string
@@ -105,5 +107,54 @@ func TestFailWrapHexAddress(t *testing.T) {
 			}
 			assert.Equal(t, tc.HexValue, result)
 		})
+	}
+}
+
+type HexAddr struct {
+	ethtypes.Address0xHex
+}
+
+func TestYamlMarshall(t *testing.T) {
+	testAddress := []struct {
+		Name     string
+		Hexvalue string `yaml:"hexvalue"`
+	}{
+		{
+			Name:     "Case 16-Bits",
+			Hexvalue: "0x1f2a000000000000000000000000000000000000",
+		},
+		{
+			Name:     "Case 24-Bits",
+			Hexvalue: "0x123abc0000000000000000000000000000000000",
+		},
+		{
+			Name:     "Case 32-Bits",
+			Hexvalue: "0xabcdeffedcba9876543210abcdeffedc00000000",
+		},
+		{
+			Name:     "Case 64-Bits",
+			Hexvalue: "0x1234567890abcdef012345670000000000000000",
+		},
+	}
+	for _, tc := range testAddress {
+		t.Run(tc.Name, func(t *testing.T) {
+			var hexType HexAddress
+			hexbyte, err := hex.DecodeString(tc.Hexvalue[2:])
+			if err != nil {
+				t.Log("unable to decode values")
+			}
+			var hexArray [20]byte
+			copy(hexArray[:], hexbyte)
+			YamlHex, err := hexType.WrapHexAddress([20]byte(hexArray))
+			if err != nil {
+				t.Log("unable to generate yaml string")
+			}
+
+			YamlNode := yaml.Node{
+				Value: YamlHex,
+			}
+			assert.YAMLEq(t, YamlNode.Value, YamlHex)
+		})
+
 	}
 }
