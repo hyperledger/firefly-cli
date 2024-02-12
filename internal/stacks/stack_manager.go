36259c30188509d2f2998ac85fb28d1171084af3
Philip-21 <philipuzomaobiora@gmail.com>
2024-02-12 22:22:45 +0100

    adjustments
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- internal/stacks/stack_manager.go
+++ internal/stacks/stack_manager.go
@@ -188,7 +188,7 @@ func (s *StackManager) InitStack(options *types.InitOptions) (err error) {
 	if err := s.writeDockerComposeOverride(compose); err != nil {
 		return fmt.Errorf("failed to write docker-compose.override.yml: %s", err)
 	}
-	if options.CustomPath == "docker-compose.yml" {
+	if options.CustomPath != "" {
 		if err := s.copyToDockerComposeOverride(options.CustomPath); err != nil {
 			return fmt.Errorf("failed to copy file data to docker-compose.override.yml: %s", err)
 		}
